b10202318ae6c287d405677801f170341746e14d
Asiyenur Yılmaz <asiyenurylmz17@gmail.com>
2019-12-22 03:08:27 +0300

    no message

---
+++ library/src/main/java/com/tutorial/library/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.tutorial.library.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tutorial.library.entity.BookEntity;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+	Optional<BookEntity> findByName(String name);
+	Optional<BookEntity> findById(int id);
+}
